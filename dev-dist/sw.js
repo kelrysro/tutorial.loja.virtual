429eee61556bd90679c0f6649769547b3a4e671b
kelrysro <kelrysilvarodrigues@gmail.com>
2024-07-26 12:37:30 -0300

    add de dados e modal

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.66scsa5kg7g"
+    "revision": "0.krna1lgq188"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
